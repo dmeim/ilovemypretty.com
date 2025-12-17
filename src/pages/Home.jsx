d2cee925fdf3660ceb113e6e08b454d4103f481c
Dimitri Meimaridis <github@dmeim.com>
2025-12-17 15:52:43 -0500

    updated home messaging

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
         </div>
         
         <h1 className="home-title">
-          Hello, My Pretty
+          My Pretty 🤍
         </h1>
         
         <p className="home-subtitle">
@@ -25,25 +25,19 @@ function Home() {
       <div className="home-message">
         <div className="home-card">
           <p>
-            I made this website as a gift for you. Whenever you're feeling 
-            something—happy, sad, anxious, or anything in between—just scan 
-            one of the cards I gave you, and you'll find a message from me 
+            I wanted my gift to you to be something custom and personal, so I decided why not make a website.
+            No matter what you are feeling, you can either scan one of the cards I gave you, 
+            or come straight to this website, and get a message from me 
             along with some comforting words from Scripture.
           </p>
           <p>
             This is my way of being there for you, even when I can't be 
-            there in person. I love you so much.
+            there in person. I love you My Pretty 🤍.
           </p>
           <p className="home-signature">
-            — With all my love 🤍
+            — Your Handsome, Swole, Nerdy Man
           </p>
         </div>
-      </div>
-      
-      <div className="home-actions">
-        <Link to="/feeling" className="home-button">
-          How are you feeling?
-        </Link>
       </div>
     </div>
   )
